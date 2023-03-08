9fa59ef2fe4fcfc05c85d68d9f0ce7a116fe4d1d
MeghaRathi-ee <75435222+MeghaRathi-ee@users.noreply.github.com>
2023-03-08 11:04:29 +0530

    Add files via upload

---
+++ icici.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.icicibank.com/about-us/article/news-icici-bank-marginal-cost-of-funds-based-lending-rate-mclr--20163004123526382"
+
+r = requests.get(url)
+
+list_header = []
+data = []
+
+soup=BeautifulSoup(r.text,"html.parser")
+table = soup.find_all('table')[0].find("tr")
+
+for items in table:
+    try:
+        list_header.append(items.get_text())
+    except:
+        continue
+
+HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+for element in HTML_data:
+    sub_data = []
+    for sub_element in element:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
+dataFrame = pd.DataFrame(data = data, columns = list_header)
+dataFrame.to_csv('icici.csv')
