3c68216a30b897fba64e4d6ed0a7bc2b6d845d27
MeghaRathi-ee <75435222+MeghaRathi-ee@users.noreply.github.com>
2023-03-08 11:13:49 +0530

    Add files via upload

---
+++ hdfc.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+url = "https://www.hdfcbank.com/personal/resources/rates"
+
+r = requests.get(url)
+
+list_header = []
+data = []
+
+soup=BeautifulSoup(r.text,"html.parser")
+# print(soup.encode("utf-8"))
+
+# id = document.querySelectorAll("[data-pid='widget-hdfc-invest-advisory-9174617'] table")[0].find('tr')
+div = soup.find_all('div', attrs={"data-pid":"widget-hdfc-invest-advisory-9174617"})[0]
+
+table = div.find("table")
+# print(table.find("tr"))
+heading = table.find("tr")
+
+for items in heading:
+    try:
+        list_header.append(items.get_text())
+    except:
+        continue
+
+HTML_data = table.find_all("tr")[1:]
+
+for element in HTML_data:
+    sub_data = []
+    for sub_element in element:
+        try:
+            sub_data.append(sub_element.get_text())
+        except:
+            continue
+    data.append(sub_data)
+
+
+
+dataFrame = pd.DataFrame(data = data, columns = list_header)
+dataFrame.to_csv('hdfc.csv')
+
+# Extract MCLR rates
+mclr_table = table
+mclr_rows = HTML_data  # skip header row
+mclr_data = {}
+for row in mclr_rows:
+    cols = row.find_all("td")
+    tenor = cols[0].text.strip()
+    rate = cols[1].text.strip()
+    mclr_data[tenor] = rate
+
+# Print MCLR rates
+print("HDFC MCLR Rates:")
+for tenor, rate in mclr_data.items():
+    print(tenor, ":", rate)
